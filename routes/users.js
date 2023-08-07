7959e6fcc7e369678fdbee4f17389ad9b8b72b13
VAIDAS SIMKUS <VAIDAS.SIMKUS@YAHOO.COM>
2023-08-07 12:36:36 +0100

    updated user route

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,35 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 const User = require('../models/User');
 
 // Register new user
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     const { username, password } = req.body;
 
-    const newUser = new User({ username, password });
-    newUser.save()
-    .then(user => {
-        res.json(user);
-    })
-    .catch(err => {
+    // Input validation
+    if (!username || !password) {
+        return res.status(400).json({ error: 'Username and password are required' });
+    }
+
+    // Check if the user already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+        return res.status(400).json({ error: 'Username already exists' });
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Save the user to the database
+    const newUser = new User({ username, password: hashedPassword });
+    try {
+        const savedUser = await newUser.save();
+        res.json({ id: savedUser._id, username: savedUser.username });
+    } catch (err) {
         res.status(500).json({ error: 'Error registering user.' });
-    });
+    }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,22 +4,29 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 const usersRouter = require('./routes/users');
 
 app.use(cors());
 app.use(express.json());
 
+// Use the users routes
+app.use('/users', usersRouter);
+
 app.get('/', (req, res) => {
   res.send('Hello, there!')
 });
 
+// Basic error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
-
-app.use('/users', usersRouter); 
 
 mongoose.connect(process.env.DATA_BASE, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connection established successfully"))
